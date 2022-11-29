d04795a3514f3c9fbe81b395a2838a69aab47e5f
muse_stargazer <bertadams00@gmail.com>
2022-11-29 08:20:31 +0000

    trying to add event listener to show pieces as selected on click

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 //code for checkers game
 
 function freshBoard () {
-  [...document.getElementsByClassName('dark piece')].forEach(div => div.classList.add('red'));
-  [...document.getElementsByClassName('dark piece2')].forEach(div => div.classList.add('black'));
+  [...document.getElementsByClassName('piece')].forEach(div => div.classList.add('red'));
+  [...document.getElementsByClassName('piece2')].forEach(div => div.classList.add('black'));
 }
 
+[...document.getElementsByClassName('red')].forEach(div => div.addEventListener("click", select));
+
+function select () {
+  document.getElementsByClassName('red').style.border= '2px black';
+}
