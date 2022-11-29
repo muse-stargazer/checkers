e53093c43a9949a13c442b25fb0e1bcf713401a0
muse_stargazer <bertadams00@gmail.com>
2022-11-29 05:56:11 +0000

    trying to start on JS but having issues getting first onclick function for freshBoard to work

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 //code for checkers game
 
+document.getElementsByClassName("newGame").onclick = function() {freshBoard()};
+
+function freshBoard () {
+  if (div.classList.contains("piece")) {
+    div.classList.add("red");
+  }
+  if (div.classList.contains("piece2")) {
+    div.classList.add("black");
+  }
+}
