3a27f4b8bd4dc2010250887d2e8056bfc1eb05cb
muse_stargazer <bertadams00@gmail.com>
2022-11-29 07:00:24 +0000

    messing with JS, changed newGameButton div to a button element

--- scripts.js
+++ scripts.js
@@ -1,15 +1,9 @@
 //code for checkers game
 
-document.getElementById("newGameButton").onclick = function() {freshBoard()};
+document.getElementsByClassName("newGame").onclick = function() {freshBoard()};
 
 function freshBoard () {
-  if (element.classList.contains("piece")) {
-    element.classList.add("red");
-    element.classList.remove("piece");
-  }
-  if (element.classList.contains("piece2")) {
-    element.classList.add("black");
-    element.classList.remove("piece2");
-  }
+  [...document.getElementsByClassName('piece')].forEach(div => classList.add('red'));
+  [...document.getElementsByClassName('piece2')].forEach(div => classList.add('black'));
 }
 
