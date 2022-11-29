38a1282400dbc5ac17805503a459baa3d791622e
muse_stargazer <bertadams00@gmail.com>
2022-11-29 07:28:53 +0000

    got newGame function working

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 //code for checkers game
 
-document.getElementsByClassName("newGame").onclick = function() {freshBoard()};
-
 function freshBoard () {
-  [...document.getElementsByClassName('piece')].forEach(div => classList.add('red'));
-  [...document.getElementsByClassName('piece2')].forEach(div => classList.add('black'));
+  [...document.getElementsByClassName('dark piece')].forEach(div => div.classList.add('red'));
+  [...document.getElementsByClassName('dark piece2')].forEach(div => div.classList.add('black'));
 }
 
