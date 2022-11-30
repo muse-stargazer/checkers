ff89c17b2f4152e390e2dd8c6e01ff98d3bc9827
muse_stargazer <bertadams00@gmail.com>
2022-11-30 01:12:31 +0000

    futzing with js

--- scripts.js
+++ scripts.js
@@ -5,14 +5,18 @@
 
 //attached to #newGameButton currently, should eventually be attached to .newGame
 //makes pieces populate at correct starting positions, marked by blank piece classes
-function freshBoard () {
+function freshBoard() {
   [...document.getElementsByClassName('piece')].forEach(div => div.classList.add('red'));
   [...document.getElementsByClassName('piece2')].forEach(div => div.classList.add('black'));
 }
 
-//trying to add visual selector to space when piece is selected
-[...document.getElementsByClassName('red')].forEach(div => div.addEventListener('click', select));
+redPieces = document.getElementsByClassName('red'); //collection of red pieces
 
-function select () {
-  document.getElementsByClassName('red').onclick.classList.add("choose");
-}
+blackPieces = document.getElementsByClassName('black'); //collection of black pieces
+
+//trying to add visual selector to space when piece is selected
+redPieces.forEach(div => {
+  div.addEventListener('click', select => {
+    classList.add('choose');
+  })
+});
