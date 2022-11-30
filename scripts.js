9017b1a2c878e5a0e42701a2986fdf9791c81dd8
muse_stargazer <bertadams00@gmail.com>
2022-11-30 00:09:54 +0000

    adding notes and testing selector function --to no avail

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
 //code for checkers game
 
+//function to apply freshBoard to all newGame class --not currently working, so have function in html
+//[...document.getElementsByClassName('newGame')].onclick = freshBoard;
+
+//attached to #newGameButton currently, should eventually be attached to .newGame
+//makes pieces populate at correct starting positions, marked by blank piece classes
 function freshBoard () {
   [...document.getElementsByClassName('piece')].forEach(div => div.classList.add('red'));
   [...document.getElementsByClassName('piece2')].forEach(div => div.classList.add('black'));
 }
 
-[...document.getElementsByClassName('red')].forEach(div => div.addEventListener("click", select));
+//trying to add visual selector to space when piece is selected
+[...document.getElementsByClassName('red')].forEach(div => div.addEventListener('click', select));
 
 function select () {
-  document.getElementsByClassName('red').style.border= '2px black';
+  document.getElementsByClassName('red').onclick.classList.add("choose");
 }
