2d35fbf4d4748147074249b68160e6d55794daa8
muse_stargazer <bertadams00@gmail.com>
2022-12-01 21:13:49 +0000

    choose class working now, thank you to Tor

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 //code for checkers game
 
+//null all global variables here
+var redPieces = null;
+var blackPieces = null;
+
 //function to apply freshBoard to all newGame class --not currently working, so have function in html--------------------
 //[...document.getElementsByClassName('newGame')].onclick = freshBoard;
 
@@ -8,20 +12,17 @@
 function freshBoard() {
   [...document.getElementsByClassName('piece')].forEach(div => div.classList.add('red'));
   [...document.getElementsByClassName('piece2')].forEach(div => div.classList.add('black'));
+
+  redPieces = document.getElementsByClassName('red'); //collection of red pieces
+
+  blackPieces = document.getElementsByClassName('black'); //collection of black pieces
+
+  //add visual selector to space when red piece is selected 
+  [...redPieces].forEach(div => div.addEventListener('click', function () {
+    this.classList.add('choose');
+  }));
+
+  redCount = redPieces.length; //number of red pieces left
+
+  blackCount = blackPieces.length; //number of black pieces left
 }
-
-redPieces = document.getElementsByClassName('red'); //collection of red pieces
-
-blackPieces = document.getElementsByClassName('black'); //collection of black pieces
-
-//trying to add visual selector to space when piece is selected --not working---------------------------
-redPieces.forEach(div => {
-  div.addEventListener('click', select => {
-    classList.add('choose');
-  })
-});
-
-redCount = redPieces.length; //number of red pieces left
-
-blackCount = blackPieces.length; //number of black pieces left
-
