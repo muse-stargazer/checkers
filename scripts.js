6c6b8e8c72fa3e5cf20438a4fa17879e7506336e
muse_stargazer <bertadams00@gmail.com>
2022-12-01 22:12:53 +0000

    starting on space counter, or trying to

--- scripts.js
+++ scripts.js
@@ -7,22 +7,34 @@ var blackPieces = null;
 //function to apply freshBoard to all newGame class --not currently working, so have function in html--------------------
 //[...document.getElementsByClassName('newGame')].onclick = freshBoard;
 
-//attached to #newGameButton currently, should eventually be attached to .newGame
-//makes pieces populate at correct starting positions, marked by blank piece classes
+//attached to #newGameButton currently, should eventually be attached to .newGame 
 function freshBoard() {
-  [...document.getElementsByClassName('piece')].forEach(div => div.classList.add('red'));
+  [...document.getElementsByClassName('piece')].forEach(div => div.classList.add('red')); //makes pieces populate at correct starting positions, marked by blank piece classes
   [...document.getElementsByClassName('piece2')].forEach(div => div.classList.add('black'));
+  [...document.getElementsByClassName('blank')].forEach(div => div.classList.remove('red', 'black')); //clears middle rows so only correct number of pieces show
 
   redPieces = document.getElementsByClassName('red'); //collection of red pieces
-
   blackPieces = document.getElementsByClassName('black'); //collection of black pieces
 
   //add visual selector to space when red piece is selected 
   [...redPieces].forEach(div => div.addEventListener('click', function () {
-    this.classList.add('choose');
+    this.classList.toggle('choose');
+    //once function is complete copy it for black pieces -----------------------------------
   }));
 
+  
+  
   redCount = redPieces.length; //number of red pieces left
-
   blackCount = blackPieces.length; //number of black pieces left
+  
+  //set up counter for chosen pieces
+  chooseCount = document.querySelectorAll('choose'); //collection of selected spaces
+  
+  function spaceCount(what) {
+    if (!what.classList.contains('choose')) {
+      
+    }
+  }
+
+
 }
