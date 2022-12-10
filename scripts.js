2f6aa4617b4cfe7eb6b75b69257b1fee706705f0
muse_stargazer <bertadams00@gmail.com>
2022-12-10 06:52:29 +0000

    moving event listeners out of freshboard

--- scripts.js
+++ scripts.js
@@ -4,9 +4,8 @@
 var redPieces = null;
 var blackPieces = null;
 var chooseCount = null;
-var darkSpaces = null;
+var darkSpaces = document.getElementsByClassName('dark'); //collection of all dark spaces (moveable spots)
 var chosenSpace = null;
-var chosenWasRed = null;
 var redCount = null;
 var blackCount = null;
 
@@ -23,62 +22,114 @@ function freshBoard() {
 
   redPieces = document.getElementsByClassName('red'); //collection of red pieces
   blackPieces = document.getElementsByClassName('black'); //collection of black pieces
-  darkSpaces = document.getElementsByClassName('dark'); //collection of all dark spaces (moveable spots)
 
   [...darkSpaces].forEach(div => div.classList.remove('choose')); //clear all selections on New Game
-
-  //add visual selector to space 
-  [...darkSpaces].forEach(div => div.addEventListener('click', function select() {
-
-    chosenSpace = document.querySelector('.choose');
-    //if we haven't selected a space yet
-    if (!chosenSpace) {
-      if (!this.classList.contains('red') && !this.classList.contains('black')) {
-        //if the space we select is empty, do nothing
-        return;
-      }
-      //the space has a piece, add .choose
-      this.classList.add('choose');
-    } else { //we have selected a piece already
-      if (this.classList.contains('red') || this.classList.contains('black')) {
-        //there is a piece on second space, invalid move
-        chosenSpace.classList.remove('choose');
-        return;
-      }
-
-      //we clicked a dark space with a piece on it, and second space with no piece: valid move -if within proximity-
-      chosenWasRed = chosenSpace.classList.contains('red'); //piece we chose was red
-      this.classList.add(chosenWasRed ? 'red' : 'black'); //if the piece was red, make new space red; if not, make new space black
-      chosenWasCrown = chosenSpace.classList.contains('crown'); //piece we chose has crown
-      if (chosenWasCrown) {
-        this.classList.add('crown');
-        chosenSpace.classList.remove('crown'); //ensure crown class moves with piece
-      }
-      chosenSpace.classList.remove('choose'); //remove choose class 
-      chosenSpace.classList.remove(chosenWasRed ? 'red' : 'black'); //remove piece from original position
-
-
-      //add crown classes
-      [...document.getElementsByClassName('red rowH')].forEach(div => div.classList.add('crown'));
-      [...document.getElementsByClassName('black rowA')].forEach(div => div.classList.add('crown'));
-      
-      
-      //if a piece was jumped, run function 'jump'
-
-
-    } //end of first click was piece
-
-
-    redCount = redPieces.length; //number of red pieces left
-    blackCount = blackPieces.length; //number of black pieces left
-
-    //when redCount=0, Black wins and vice versa
-    if (redCount == 0) {
-      alert('Black Wins!');
-    }
-    if (blackCount == 0) {
-      alert('Red Wins!');
-    }
-
-  })) //end of select function on dark spaces
 } //end of freshBoard
+
+//add visual selector to space 
+[...darkSpaces].forEach(div => div.addEventListener('click', function select() {
+
+  chosenSpace = document.querySelector('.choose');
+
+  //if we haven't selected a space yet
+  if (!chosenSpace) {
+    if (!this.classList.contains('red') && !this.classList.contains('black')) {
+      //if the space we select is empty, do nothing
+      return;
+    }
+    //the space has a piece, add .choose
+    this.classList.add('choose');
+  } else {
+    //we have selected a piece already
+
+    if (this.classList.contains('red') || this.classList.contains('black')) {
+      //there is a piece on second space, invalid move
+      chosenSpace.classList.remove('choose');
+      return;
+    }
+
+    //we clicked a dark space with a piece on it, and second space with no piece: valid move -if within proximity-
+    var chosenWasRed = chosenSpace.classList.contains('red'); //piece we chose was red
+    var chosenWasCrown = chosenSpace.classList.contains('crown'); //piece we chose has crown    
+
+    /** TED CODE */
+    const fromRow = +chosenSpace.dataset.row;
+    const fromCol = +chosenSpace.dataset.col;
+
+    const toRow = +this.dataset.row;
+    const toCol = +this.dataset.col;
+
+    if (chosenWasRed) {
+      // Red pieces move to higher rows
+      if (toRow !== fromRow + 1 || (toCol !== fromCol + 1 && toCol !== fromCol - 1)) {
+        // Make exception for skips
+        if (toRow === fromRow + 2 && toCol === fromCol + 2 && document.querySelector('[data-row="' + (fromRow + 1) + '"][data-col="' + (fromCol + 1)  + '"].black')) {
+          // WE SKIPPIN YA'LL 
+        } else if (toRow === fromRow + 2 && toCol === fromCol - 2 && document.querySelector('[data-row="' + (fromRow + 1) + '"][data-col="' + (fromCol - 1)  + '"].black')) {
+          // We are skipping
+        } else {
+          return;
+        }
+      }
+    } else {
+      // Vlack pieces move to lower rows
+      if (toRow !== fromRow - 1 || (toCol !== fromCol + 1 && toCol !== fromCol - 1)) {
+        // Make exception for skips
+        if (toRow === fromRow - 2 && toCol === fromCol + 2 && document.querySelector('[data-row="' + (fromRow - 1) + '"][data-col="' + (fromCol + 1)  + '"].red')) {
+          // WE SKIPPIN YA'LL 
+        } else if (toRow === fromRow - 2 && toCol === fromCol - 2 && document.querySelector('[data-row="' + (fromRow - 1) + '"][data-col="' + (fromCol - 1)  + '"].red')) {
+          // We are skipping
+        } else {
+          return;
+        }
+      }
+    }
+
+
+    /**
+    
+    
+    
+    */
+
+    /** END TED CODE */
+
+
+    this.classList.add(chosenWasRed ? 'red' : 'black'); //if the piece was red, make new space red; if not, make new space black
+
+
+
+    if (chosenWasCrown) {
+      this.classList.add('crown');
+      chosenSpace.classList.remove('crown'); //ensure crown class moves with piece
+    }
+    chosenSpace.classList.remove('choose'); //remove choose class 
+    chosenSpace.classList.remove(chosenWasRed ? 'red' : 'black'); //remove piece from original position
+
+
+    //add crown classes
+    [...document.getElementsByClassName('red rowH')].forEach(div => div.classList.add('crown'));
+    [...document.getElementsByClassName('black rowA')].forEach(div => div.classList.add('crown'));
+
+
+    //if a piece was jumped, run function 'jump'
+
+
+  } //end of first click was piece
+
+
+  redCount = redPieces.length; //number of red pieces left
+  blackCount = blackPieces.length; //number of black pieces left
+
+  //when redCount=0, Black wins and vice versa
+  if (redCount == 0) {
+    alert('Black Wins!');
+  }
+  if (blackCount == 0) {
+    alert('Red Wins!');
+  }
+
+})) //end of select function on dark spaces
+
+
+freshBoard();
